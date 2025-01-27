152f5aacaf396dda31963db9961b3f22bd968796
Andrew Chou <andrewchou@fastmail.com>
2025-01-27 15:29:54 -0500

    update application

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  applicationId,
+  nativeApplicationVersion,
+  nativeBuildVersion,
+} from "expo-application";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <View style={styles.row}>
+        <Text style={styles.bold}>Application ID:</Text>
+        <Text>{applicationId}</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.bold}>Application Version:</Text>
+        <Text>{nativeApplicationVersion}</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.bold}>Build Version:</Text>
+        <Text>{nativeBuildVersion}</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +30,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    padding: 20,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  bold: {
+    fontWeight: "bold",
   },
 });
