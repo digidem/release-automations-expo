16d4c662ca3cd2dbc527b8f5ee54bb682b3a1659
Andrew Chou <andrewchou@fastmail.com>
2025-01-30 16:12:00 -0500

    UI improvements

--- App.tsx
+++ App.tsx
@@ -1,42 +1,49 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import {
   applicationId,
+  applicationName,
   nativeApplicationVersion,
   nativeBuildVersion,
 } from "expo-application";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="dark" />
 
-      <View style={styles.row}>
-        <Text style={styles.bold}>Application ID:</Text>
-        <Text>{applicationId}</Text>
+      <Text style={[styles.title, styles.bold]}>{applicationName}</Text>
+
+      <View style={styles.infoContainer}>
+        <View>
+          <Text style={styles.bold}>Application ID:</Text>
+          <Text>{applicationId}</Text>
+        </View>
+        <View>
+          <Text style={styles.bold}>Application Version:</Text>
+          <Text>{nativeApplicationVersion}</Text>
+        </View>
+        <View>
+          <Text style={styles.bold}>Build Version:</Text>
+          <Text>{nativeBuildVersion}</Text>
+        </View>
       </View>
-      <View style={styles.row}>
-        <Text style={styles.bold}>Application Version:</Text>
-        <Text>{nativeApplicationVersion}</Text>
-      </View>
-      <View style={styles.row}>
-        <Text style={styles.bold}>Build Version:</Text>
-        <Text>{nativeBuildVersion}</Text>
-      </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    padding: 20,
+    paddingVertical: 80,
+    paddingHorizontal: 20,
+    gap: 40,
   },
-  row: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+  infoContainer: {
+    gap: 20,
+  },
+  title: {
+    fontSize: 24,
   },
   bold: {
     fontWeight: "bold",
